2cdb8461fd2fef266f5cd66f9647b6d2f35fa338
Burned357Waffles <burned357waffles@protonmail.com>
2023-03-20 19:42:29 -0700

    camera has boundary and zoom limit
    Still working on the angle change on zoom

---
+++ Assets/RedBjorn/Utils/Runtime/Extensions/RandomExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RedBjorn.SuperTiles
+{
+    public static class RandomExtensions
+    {
+        public static bool Chance(this System.Random randomizer, float probability)
+        {
+            return randomizer == null ? false : randomizer.NextDouble() <= probability;
+        }
+    }
+}
