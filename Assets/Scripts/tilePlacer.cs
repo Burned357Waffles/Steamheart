5555d05ea40fee75afc2e83dda01d9cdf38ae2e7
ClydePineda <27971415+ClydePineda@users.noreply.github.com>
2023-03-28 17:50:34 -0700

    slight change in SampleScene

--- Assets/Scripts/tilePlacer.cs
+++ Assets/Scripts/tilePlacer.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class tilePlacer : MonoBehaviour
 {
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
+
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
+
 }
