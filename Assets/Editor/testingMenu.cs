28c55a24b8029527daa78ec561470035fc58ebc2
Jasmine Hart <jasminestapletonhart@gmail.com>
2023-03-31 14:19:53 -0700

    More work on menu layout

--- Assets/Editor/testingMenu.cs
+++ Assets/Editor/testingMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuManager : MonoBehaviour
+public class testingMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
