9aa62635ecfd2c456bc0b1e8c7ebd3235bbfe500
Burned357Waffles <bwatanabe@sfsu.edu>
2023-04-06 23:48:39 -0700

    writing new map gen

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -6,7 +6,16 @@ public class HexGrid : MonoBehaviour
 {
     Dictionary<Vector3Int, Hex> hexTileDict = new Dictionary<Vector3Int, Hex>();
     Dictionary<Vector3Int, List<Vector3Int>> hexTileNeighborsDict = new Dictionary<Vector3Int, List<Vector3Int>>();
-
+    
+    
+    /// <summary>
+    ///  Temp code
+    /// </summary>
+    [SerializeField] private GameObject hex; // Temp
+    [SerializeField] private GameObject water_hex; // Temp
+    [SerializeField] private GameObject grass_hex; // Temp
+    
+    
     private void Start()
      {
 
@@ -21,7 +30,31 @@ public class HexGrid : MonoBehaviour
         {
             Debug.Log(neighborsPos);
         }
-    }
+        
+        
+        // Temp Code
+        float[] size = new float[]{10,10};
+        GameObject new_hex;
+        for(int i = 0; i < size[0]; i++)
+        {
+            for (int j = 0; j < size[1]; j++)
+            {
+                float elevation = Mathf.PerlinNoise((float)i/(size[0]*10), (float)j/(size[1] *10));
+
+                if (elevation < .5)
+                {
+                    hex = water_hex;
+                }
+                new_hex =  Instantiate(hex);
+
+                
+                new_hex.transform.position = new Vector3(i*2, elevation*10f, j);
+                new_hex.transform.parent = this.gameObject.transform;
+                Debug.Log(elevation);
+            }
+        }
+
+     }
 
     public Hex GetTileAt(Vector3Int hexCoordinates)
     {
