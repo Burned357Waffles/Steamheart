23b6eacab5317360932a3d1e1a5de017ac136ca9
ClydePineda <27971415+ClydePineda@users.noreply.github.com>
2023-04-11 18:08:23 -0700

    HexMovement.cs started, tilePlacer.cs testing

---
+++ Assets/Scripts/HexMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMovement : MonoBehaviour
+{
+    public HashSet<Hex> hex_reachable(Hex start,int movement)
+    {
+        HashSet<Hex> visited = new HashSet<Hex>();
+        visited.Add(start);
+
+        List<List<Hex>> fringes = new List<List<Hex>>();
+        fringes.Add(new List<Hex> { start });
+
+        for (int i = 1; i <= movement; i++)
+        {
+            fringes.Add(new List<Hex>());
+            
+        }
+    }
+
+}
--- Assets/Scripts/tilePlacer.cs
+++ Assets/Scripts/tilePlacer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 // this will convert an air tile to a land tile
 public class tilePlacer : MonoBehaviour
 {
-    
+    public GameObject tile;
     // Start is called before the first frame update
     void Start() {
 
@@ -14,7 +14,12 @@ public class tilePlacer : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("mouse1 pressed");
+            //Instantiate(tile, transform.position, transform.rotation);
+        }
+        
     }
 
 }
