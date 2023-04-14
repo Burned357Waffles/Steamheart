3e338df19f98c43012171a70a3fa5030ff7750cb
ClydePineda <27971415+ClydePineda@users.noreply.github.com>
2023-04-13 19:43:38 -0700

    minor changes, HexMovement.cs still has error

--- Assets/Scripts/HexMovement.cs
+++ Assets/Scripts/HexMovement.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class HexMovement : MonoBehaviour
 {
-    // public HashSet<Hex> hex_reachable(Hex start,int movement)
-    // {
-    //     HashSet<Hex> visited = new HashSet<Hex>();
-    //     visited.Add(start);
+    public HashSet<Hex> hex_reachable(Hex start,int movement)
+    {
+        HashSet<Hex> visited = new HashSet<Hex>();
+        visited.Add(start);
 
-    //     List<List<Hex>> fringes = new List<List<Hex>>();
-    //     fringes.Add(new List<Hex> { start });
+        List<List<Hex>> fringes = new List<List<Hex>>();
+        fringes.Add(new List<Hex> { start });
 
-    //     for (int i = 1; i <= movement; i++)
-    //     {
-    //         fringes.Add(new List<Hex>());
+        for (int i = 1; i <= movement; i++)
+        {
+            fringes.Add(new List<Hex>());
+            foreach(Hex hex in fringes[i-1])
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    // Hex neighbor = HexNeighbor(hex, j);
+                    // if (!visited.Contains(neighbor) && !blocked)
+                    // {
+                    //     visited.Add(neighbor);
+                    //     fringes[i].Add(neighbor);
+                    // }
+                }
+            }
             
-    //     }
-    // }
+        }
+        return visited;
+    }
+
+    
 
 }
