d22c8a9b325a08f4b0d56ed047283b6f81357c79
Burned357Waffles <burned357waffles@protonmail.com>
2023-05-08 18:55:31 -0700

    pause menu works and can exit game

---
+++ Assets/Scripts/UI/HUD/SettingsMenu.cs
@@ -0,0 +1,41 @@
+﻿using Misc;
+using UnityEngine;
+
+namespace UI.HUD
+{
+    public class SettingsMenu : MonoBehaviour
+    {
+        [SerializeField] public GameObject cameraRig;
+        [SerializeField] public Canvas settingsCanvas;
+
+        private void Start()
+        {
+            settingsCanvas.enabled = false;
+        }
+
+        public void OpenMenu()
+        {
+            cameraRig.GetComponent<MoveCamera>().enabled = false;
+            settingsCanvas.enabled = true;
+        }
+
+        public void Resume()
+        {
+            cameraRig.GetComponent<MoveCamera>().enabled = true;
+            settingsCanvas.enabled = false;
+        }
+
+        public void Exit()
+        {
+            #if UNITY_EDITOR
+            {
+                UnityEditor.EditorApplication.isPlaying = false;
+            }
+            #else 
+		    {
+			Application.Quit();
+		    }
+            #endif
+        }
+    }
+}
