82757901933cae1f364de680d065106c0237a182
Jasmine Hart <jstapletonhart@mail.sfsu.edu>
2023-05-17 21:56:15 -0700

    Loading screen implemented
    Loading screen plays when game is started now

--- Assets/Scripts/LoadingManager.cs
+++ Assets/Scripts/LoadingManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LoadingManager : MonoBehaviour
 {
     public static LoadingManager Instance;
+    [SerializeField] private Canvas _loadingScreen;
 
     public void Awake()
     {
@@ -13,10 +15,19 @@ public class LoadingManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+
+        _loadingScreen = GameObject.Find("Loading Screen").GetComponent<Canvas>();
     }
 
     public void LoadScene(string sceneName)
     {
+        _loadingScreen.enabled = true;
+        StartCoroutine(LoadingScreen(sceneName));
+    }
+
+    public IEnumerator LoadingScreen(string sceneName)
+    {
+        yield return new WaitForSeconds(2);
         SceneManager.LoadScene(sceneName);
     }
 }
